c451dbed5a302ffb85fd3fb1c9df67ac61f4b27c
MaheshKummari <maheshkummari7111@gmail.com>
2024-04-26 22:47:26 +0530

    contact form functionality added

--- src/components/utils/index.js
+++ src/components/utils/index.js
@@ -1,35 +1,37 @@
-import Banner from './Banner';
-import CartItem from './CartItem';
-import CartItemsContainer from './CartItemsContainer';
-import Categories from './Categories';
-import Coupon from './Coupon';
-import DealOfTheDay from './DealOfTheDay';
-import PageNotFound from './PageNotFound';
-import Partners from './Partners';
-import ProductCard from './ProductCard';
-import Products from './Products';
-import Search from './Search';
-import ShowProducts from './ShowProducts';
-import Spinner from './Spinner';
-import Testimonial from './Testimonial';
-import Testimonials from './Testimonials';
-import requestServer from './requestServer';
+import Banner from "./Banner";
+import CartItem from "./CartItem";
+import CartItemsContainer from "./CartItemsContainer";
+import Categories from "./Categories";
+import Coupon from "./Coupon";
+import DealOfTheDay from "./DealOfTheDay";
+import PageNotFound from "./PageNotFound";
+import Partners from "./Partners";
+import ProductCard from "./ProductCard";
+import Products from "./Products";
+import Search from "./Search";
+import ShowProducts from "./ShowProducts";
+import Spinner from "./Spinner";
+import Testimonial from "./Testimonial";
+import Testimonials from "./Testimonials";
+import requestServer from "./requestServer";
+import GoogleMap from "./GoogleMap";
 
 export {
-	Categories,
-	DealOfTheDay,
-	PageNotFound,
-	Partners,
-	Search,
-	Spinner,
-	requestServer,
-	Products,
-	ProductCard,
-	ShowProducts,
-	Testimonial,
-	Testimonials,
-	Banner,
-	CartItem,
-	CartItemsContainer,
-	Coupon,
+  Categories,
+  DealOfTheDay,
+  PageNotFound,
+  Partners,
+  Search,
+  Spinner,
+  requestServer,
+  Products,
+  ProductCard,
+  ShowProducts,
+  Testimonial,
+  Testimonials,
+  Banner,
+  CartItem,
+  CartItemsContainer,
+  Coupon,
+  GoogleMap,
 };
--- src/components/utils/requestServer.js
+++ src/components/utils/requestServer.js
@@ -1,24 +1,24 @@
-const requestServer = async (url, options = { method: 'GET' }) => {
-	if (url.startsWith('/')) {
-		url = `https://veggie-bazaar-backend-mongodb.vercel.app${url}`;
-		// url = `http://localhost:7200${url}`;
-	}
-	console.log(url);
-	console.log(options);
-	try {
-		const response = await fetch(url, options);
-		if (!response.ok) {
-			console.log(response);
-			throw Error(
-				`Error Occured while Fetching with satus code ${response.status} -> ${response.statusText}`
-			);
-		}
-		const data = await response.json();
-		console.log('responsedata', data);
-		return data;
-	} catch (error) {
-		console.log(error.message);
-	}
+const requestServer = async (url, options = { method: "GET" }) => {
+  if (url.startsWith("/")) {
+    // url = `https://veggie-bazaar-backend-mongodb.vercel.app${url}`;
+    url = `http://localhost:7200${url}`;
+  }
+  console.log(url);
+  console.log(options);
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      console.log(response);
+      throw Error(
+        `Error Occured while Fetching with satus code ${response.status} -> ${response.statusText}`
+      );
+    }
+    const data = await response.json();
+    console.log("responsedata", data);
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 export default requestServer;
