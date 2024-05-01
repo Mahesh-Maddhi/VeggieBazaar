75ff1ac2c1e862b50dbbaee30cf3679c70817b01
Mahesh Maddhi <maddimahesh111@gmail.com>
2024-05-01 14:53:27 +0530

    currency symbols changed

--- src/components/utils/CartItemsContainer.js
+++ src/components/utils/CartItemsContainer.js
@@ -110,21 +110,21 @@ const CartItemsContainer = () => {
 						<h3 className="billing-heading mb-4">Cart Total</h3>
 						<p className="d-flex">
 							<span className="mr-3">Subtotal</span>
-							<span className="text-right w-100">$20.60</span>
+							<span className="text-right w-100">₹60.60</span>
 						</p>
 						<p className="d-flex">
 							<span className="mr-3">Delivery</span>
-							<span className="text-right  w-100">$0.00</span>
+							<span className="text-right  w-100">₹0.00</span>
 						</p>
 						<p className="d-flex">
 							<span className="mr-3">Discount</span>
-							<span className="text-right  w-100">$3.00</span>
+							<span className="text-right  w-100">₹3.00</span>
 						</p>
 						<hr />
 						<p className="d-flex total-price">
 							<span className="mr-3 font-weight-bold">Total</span>
 							<span className="text-right  w-100 font-weight-bold">
-								$17.60
+								₹57.60
 							</span>
 						</p>
 						<p className="text-center">
